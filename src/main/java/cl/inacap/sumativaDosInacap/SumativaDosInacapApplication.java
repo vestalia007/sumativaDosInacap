a803467c5f4ea2ac4fedfb13a0e0738d2d6a3a1c
kmontero16 <karla.monter01@gmail.com>
2022-06-09 18:16:19 -0400

    Primera Carga

---
+++ src/main/java/cl/inacap/sumativaDosInacap/SumativaDosInacapApplication.java
@@ -0,0 +1,13 @@
+package cl.inacap.sumativaDosInacap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumativaDosInacapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumativaDosInacapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/inacap/sumativaDosInacap/SumativaDosInacapApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.inacap.sumativaDosInacap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumativaDosInacapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
