47dc447f13dfb16829ec985379532c0f964c0f16
kmontero16 <karla.monter01@gmail.com>
2022-06-10 15:07:22 -0400

    Creacion de entity
    Se definen atributos y metodos de los entity

--- src/main/java/cl/inacap/sumativaDosInacap/entity/ProductoEntity.java
+++ src/main/java/cl/inacap/sumativaDosInacap/entity/ProductoEntity.java
@@ -1,5 +1,55 @@
 package cl.inacap.sumativaDosInacap.entity;
 
-public class ProductoEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="productos")
+public class ProductoEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column( length = 50, nullable = false)
+	private String nombre;
+	@Column(nullable = false)
+	private int precio;
+	@Column(length = 200, nullable = false)
+	private String caracteristicas;
+	@Column(length = 50, nullable = false)
+	private String categoria;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public String getCaracteristicas() {
+		return caracteristicas;
+	}
+	public void setCaracteristicas(String caracteristicas) {
+		this.caracteristicas = caracteristicas;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 }
--- src/main/java/cl/inacap/sumativaDosInacap/entity/UsuarioEntity.java
+++ src/main/java/cl/inacap/sumativaDosInacap/entity/UsuarioEntity.java
@@ -1,5 +1,71 @@
 package cl.inacap.sumativaDosInacap.entity;
 
-public class UsuarioEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="usuarios")
+public class UsuarioEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column( length = 50 , nullable = false)
+	private String nombre;
+	
+	@Column( length = 50 , nullable = false)
+	private String password;
+	
+	@Column( length = 50 , nullable = false)
+	private String perfil;
+	
+	@Column( nullable = true)
+	private int edad;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 }
