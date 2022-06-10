8a7cf33dd05823b2ab33b07f332b6c44e77b3451
kmontero16 <karla.monter01@gmail.com>
2022-06-10 15:14:29 -0400

    Creacion DAO
    se generan los metodos de los objetos dao de los entity

--- src/main/java/cl/inacap/sumativaDosInacap/dao/ProductoDAO.java
+++ src/main/java/cl/inacap/sumativaDosInacap/dao/ProductoDAO.java
@@ -1,5 +1,15 @@
 package cl.inacap.sumativaDosInacap.dao;
 
-public interface ProductoDAO {
+import java.util.List;
 
+import cl.inacap.sumativaDosInacap.entity.ProductoEntity;
+
+public interface ProductoDAO {
+	public void agregar(ProductoEntity u);
+	public void eliminar(int id);
+	public void modificar(ProductoEntity u);
+	public List<ProductoEntity> listar();
+	public ProductoEntity buscar(int id);
+	public ProductoEntity buscarNombre(String n);
+	public ProductoEntity buscarCategoria(String c);
 }
--- src/main/java/cl/inacap/sumativaDosInacap/dao/UsuarioDAO.java
+++ src/main/java/cl/inacap/sumativaDosInacap/dao/UsuarioDAO.java
@@ -1,5 +1,16 @@
 package cl.inacap.sumativaDosInacap.dao;
 
+import java.util.List;
+
+import cl.inacap.sumativaDosInacap.entity.UsuarioEntity;
+
 public interface UsuarioDAO {
 
+	public void agregar(UsuarioEntity u);
+	public void eliminar(int id);
+	public void eliminar(String n, String p);
+	public void modificar(UsuarioEntity u);
+	public List<UsuarioEntity> listar();
+	public UsuarioEntity buscar(int id);
+	public UsuarioEntity buscar(String n);
 }
