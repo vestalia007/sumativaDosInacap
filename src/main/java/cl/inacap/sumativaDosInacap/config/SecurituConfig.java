3d2a304f50f0194cc5e9d34094766cb53baa8719
kmontero16 <karla.monter01@gmail.com>
2022-06-10 23:15:27 -0400

    Configruacion de seguridad
    se realiza la configuracion de seguridad de la aplicacion, se vaja la version de sping boot

--- src/main/java/cl/inacap/sumativaDosInacap/config/SecurituConfig.java
+++ src/main/java/cl/inacap/sumativaDosInacap/config/SecurituConfig.java
@@ -1,5 +1,51 @@
 package cl.inacap.sumativaDosInacap.config;
 
-public class SecurituConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import cl.inacap.sumativaDosInacap.service.UsuarioSecurityService;
+
+@EnableWebSecurity
+@Configuration
+public class SecurituConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UsuarioSecurityService service;
+	
+	//@Autowired
+	//private BCryptPasswordEncoder bcpe;
+	
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder()
+	{
+		BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
+		return bcpe;
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http	
+			.headers().frameOptions().disable();
+		http
+		.csrf().disable()
+		.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+		//.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+		.authorizeRequests()
+		//.antMatchers("/api/user/**").permitAll()
+		.antMatchers("/h2-console/**").permitAll()
+		.anyRequest().authenticated();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service).passwordEncoder(passwordEncoder());
+	}
 
 }
--- src/main/java/cl/inacap/sumativaDosInacap/service/UsuarioSecurityService.java
+++ src/main/java/cl/inacap/sumativaDosInacap/service/UsuarioSecurityService.java
@@ -1,5 +1,32 @@
 package cl.inacap.sumativaDosInacap.service;
 
-public class UsuarioSecurityService {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import cl.inacap.sumativaDosInacap.dao.UsuarioDAO;
+import cl.inacap.sumativaDosInacap.entity.UsuarioEntity;
+
+@Service
+public class UsuarioSecurityService  implements UserDetailsService{
+
+	@Autowired
+	private UsuarioDAO dao;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		UsuarioEntity u = dao.buscar(username);
+		ArrayList<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+		roles.add(new SimpleGrantedAuthority("admin"));
+		UserDetails userDT = new User(u.getNombre(), u.getPassword(), roles);
+		return userDT;
+	}
 }
